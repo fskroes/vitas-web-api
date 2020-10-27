ff488c3eff37b274a6ffafa9dbad785a54759858
Fernando Silva Kroes <meanmachine200+github@gmail.com>
2020-10-27 08:55:53 +0100

    cleaned code + added multilple file upload

--- routes/upload.js
+++ routes/upload.js
@@ -1,6 +1,5 @@
-const IncomingForm = require('formidable').IncomingForm
 
-module.exports = async function(req, res) {
+module.exports = async function upload(req, res) {
     try {
         if(!req.files) {
             res.send({
@@ -9,20 +8,20 @@ module.exports = async function(req, res) {
             });
         } else {
             //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
-            console.log('file ---: ', req.files)
-            let avatar = req.files.file;
+            console.log('log file', req.files)
+            let file = req.files.file;
             
             //Use the mv() method to place the file in upload directory (i.e. "uploads")
-            avatar.mv('./public/images/' + avatar.name);
+            file.mv('./public/images/' + file.name);
 
             //send response
             res.send({
                 status: true,
                 message: 'File is uploaded',
                 data: {
-                    name: avatar.name,
-                    mimetype: avatar.mimetype,
-                    size: avatar.size
+                    name: file.name,
+                    mimetype: file.mimetype,
+                    size: file.size
                 }
             });
         }
@@ -32,22 +31,41 @@ module.exports = async function(req, res) {
     }
 }
 
-// module.exports = function upload(req, res) {
-//     var form = new IncomingForm()
+module.exports = async function multiple_upload(req, res) {
+    try {
+        if(!req.files) {
+            res.send({
+                status: false,
+                message: 'No file uploaded'
+            })
+        } 
+        else {
+            let data = []; 
+    
+            //loop all files
+            _.forEach(_.keysIn(req.files.file), (key) => {
+                let file = req.files.file[key];
+                
+                //move photo to uploads directory
+                file.mv('./public/images/' + file.name);
 
-//     console.log(req.files)
-
-//     form.on('file', (field, file) => {
-//         // do something with the file
-//         // e.g. save it to the database
-//         // you can access it using file.path
-        
-//         console.log(file.path)
-//     })
-
-//     form.on('end', () => {
-//         res.json()
-//     })
-
-//     form.parse(req)
-// }
+                //push file details
+                data.push({
+                    name: file.name,
+                    mimetype: file.mimetype,
+                    size: file.size
+                });
+            });
+    
+            //return response
+            res.send({
+                status: true,
+                message: 'Files are uploaded',
+                data: data
+            });
+        }
+    } catch (err) {
+        console.log(err)
+        res.status(500).send(err);
+    }
+}
