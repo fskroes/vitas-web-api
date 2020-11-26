442b9113b22ed2027a9a04b88828f24af7a355a5
Fernando Silva Kroes <meanmachine200+github@gmail.com>
2020-11-26 22:18:07 +0100

    trying to fix test

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -10,15 +10,16 @@ visit the Jest docs for a full list and details of jest functions.
 
 const request = require('supertest')
 // const app = require('../app')
-const app = express();
+// const express = require('express')
 const filePath = '../api/public/images/lena-small-bw1.jpg'
 
 describe('Post Endpoints', () => {
+    var app = require('../app')
     it('should create a new post', async () => {
         const res = await request(app)
         .post('/upload')
         .attach('file', filePath, { contentType: 'application/octet-stream' })
-        .expect(200)
+        // .expect(200)
 
       expect(res.statusCode).toEqual(200)
       expect(res.body).toHaveProperty('dataCV')
